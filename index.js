583ff98dd23e2f7c34fe09923d637abb80fc7e4a
tests roodx <43090686+testsRoodx@users.noreply.github.com>
2020-10-01 19:50:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 
+client.on('message', roodx=>{
+  var rr = ['hi','hello','waht','good','sad'];
+  if(roodx.content === 'hi'){
+   var embed = new Discord.RichEmbed()
+   .setColor('RANDOM')
+   .setDescription(`${rr[Math.floor(Math.random() * rr.length)]}`)
+   roodx.channel.sendEmbed(embed)
+  }
+})
+
+
 
 
 
